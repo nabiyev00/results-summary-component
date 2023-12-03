ff9fba4911d687e6256a43a6efdf03df0ef6bdd9
nabiyev00 <nabiyevshaboz0@gmail.com>
2023-12-03 21:35:44 +0500

    add img and json data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("test");
+const data = [
+  {
+    category: "Reaction",
+    score: 80,
+    icon: "../images/icon-reaction.svg",
+  },
+  {
+    category: "Memory",
+    score: 92,
+    icon: "../images/icon-memory.svg",
+  },
+  {
+    category: "Verbal",
+    score: 61,
+    icon: "../images/icon-verbal.svg",
+  },
+  {
+    category: "Visual",
+    score: 72,
+    icon: "../images/icon-visual.svg",
+  },
+];
